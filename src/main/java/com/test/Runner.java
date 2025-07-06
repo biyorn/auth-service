bed1d752d0b1ba130fecc27dd0cdd4ecc8a094aa
Pavel Arlouski <porlovski3@gmail.com>
2025-07-06 23:16:21 +0300

    Initial project

---
+++ src/main/java/com/test/Runner.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class Runner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class, args);
+    }
+}
